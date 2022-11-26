06da311f1cba1fa99159f552006ee4c447bd2fe6
omarelsabaawy <omarelsabaawy77@gmail.com>
2022-11-26 12:53:43 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,43 @@
+const data = {
+    products: [
+        {
+            name: "nike slim shirt",
+            slug: "nike-slim-shirt",
+            category: "shirts",
+            image: "/images/p1.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            desc: "nike tshirt"
+        },
+        {
+            name: "nike pants",
+            slug: "nike-men-pants",
+            category: "pants",
+            image: "/images/p3.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.4,
+            numReviews: 10,
+            desc: "nike pants"
+        },
+        {
+            name: "Normal People Sally Rooney",
+            slug: "Normal-People-Sally-Rooney",
+            category: "books",
+            image: "/images/p2.jpg",
+            price: 50,
+            countInStock: 10,
+            brand: "book",
+            rating: 4.0,
+            numReviews: 10,
+            desc: "Normal People Sally Rooney book you have to read"
+        }
+
+    ]
+}
+
+export default data;
